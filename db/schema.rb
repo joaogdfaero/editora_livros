8ab538f57c5388a35b5e126da6180c21fa0bd5a2
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-22 17:45:50 -0300

    feature(filters): added necessary columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_141315) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_202608) do
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
     t.integer "supplier_id", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_141315) do
     t.integer "supplier_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
