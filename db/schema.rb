3d156387dbaa586a935c89ea9c31b9d0d33c4c27
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-22 09:06:10 -0300

    feat(supplier): cnpj added to supplier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_103517) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_115138) do
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
     t.integer "supplier_id", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_103517) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cnpj"
   end
 
   add_foreign_key "accounts", "suppliers"
