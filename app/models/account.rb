215701774d1c6b0c0e73f6d4a34aacc673a495d2
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-22 17:07:53 -0300

    feat(validations): calculo digito verificador adicionado

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,11 @@ class Account < ApplicationRecord
   belongs_to :supplier
   validates :account_number, presence: true
   validates :account_number, uniqueness: true
+  validate :account_digit_is_valid?
+
+  private
+  def account_digit_is_valid?
+    d = CheckAccountDigit.new(account_number, digit)
+    errors.add(:digit, "Dígito verificador inválido! Por favor, verifique os dados.") unless d.check_digit?
+  end
 end
