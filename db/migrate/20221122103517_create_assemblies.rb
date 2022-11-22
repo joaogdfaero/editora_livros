e6c04d3ad11c0c3f04b9871ddbc9ac48e880325a
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-22 08:15:18 -0300

    feat(assemblies_parts): basic CRUD with assembly, part and book

---
+++ db/migrate/20221122103517_create_assemblies.rb
@@ -0,0 +1,11 @@
+class CreateAssemblies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assemblies do |t|
+      t.string :name
+      t.references :part, null: false, foreign_key: true
+      t.references :book, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
