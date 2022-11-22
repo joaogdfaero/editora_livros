20ab499e8dea99e608359e66166a5440a3e83e06
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-22 16:50:39 -0300

    feat(validations): added cpf, cnpj and isbn validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
   has_many :assemblies
   validates :title, presence: true
   validates :title, length: { minimum: 3, maximum: 100 }
+  validate :isbn_is_valid?
+
+  private
+  def isbn_is_valid?
+    errors.add(:isbn, "ISBN inválido! Por favor, verifique os dados.") unless ISBN.valid?(isbn)
+  end
 end
