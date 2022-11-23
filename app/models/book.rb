72b18a6a9aa12138122ebef31b82cebb82bff031
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-23 08:27:41 -0300

    feature(filters): books by author filter working

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,9 @@ class Book < ApplicationRecord
   validates :title, length: { minimum: 3, maximum: 100 }
   validate :isbn_is_valid?
 
+  scope :search, ->(query) { where("title like ?", "%#{query}%") }
+  scope :by_author_name, ->(query) { joins(:author).where("LOWER(authors.name) LIKE ?", "%#{query.downcase}%") }
+
   private
   def isbn_is_valid?
     errors.add(:isbn, "ISBN inválido! Por favor, verifique os dados.") unless ISBN.valid?(isbn)
