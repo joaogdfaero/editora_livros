ab4e300f221a371646617a70010471fe497b383b
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-23 08:53:15 -0300

    feature(filters): supplier by name filter working

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -5,6 +5,8 @@ class Supplier < ApplicationRecord
   validates :name, length: { minimum: 3, maximum: 60 }
   validate :cnpj_is_valid?
 
+  scope :by_supplier_name, ->(query) { where("LOWER(name) LIKE ?", "%#{query.downcase}%") }
+
   private
   def cnpj_is_valid?
     errors.add(:cnpj, "CNPJ inválido! Por favor, verifique os dados." ) unless CNPJ.valid?(cnpj)
