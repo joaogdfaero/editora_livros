bc28f181170cea75f6c5f14363a58dc50603d6ac
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-03-03 08:29:57 -0300

    feature(testes): building author CRUD tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,13 +1,41 @@
 require 'rails_helper'
 
-describe Author do 
-  it "é válido quando contém o primeiro nome" 
-  it "tamanho do primeir nome precisa ser entre 3 e 60 caracteres"
-  it "cpf precisa ser válido"
- end
+RSpec.describe Author, type: :model do
+  context "CRUD Author" do
+
+    # CREATE
+    it 'Create Author' do
+      author_type = Author.create(name: "Machado de Assis")
+      expect(author_type.name).to eq("Machado de Assis")
+    end
+
+    # UPDATE
+    it 'Update Author' do
+      author_type = Author.create(name: "Machado de Assis")
+      author_type.update(name: "de Assis, Machado")
+      expect(author_type.name).to eq("de Assis, Machado")
+    end
+
+    # FIELD NAME REQUIRED
+    describe 'Required validations' do
+     it { should validate_presence_of(:name) }
+    end
+
+    # MIN AND MAX NAME LENGTH REQUIRED
+    describe 'Maximum field size' do
+      it { should validate_length_of(:name).is_at_least(3) }
+      it { should validate_length_of(:name).is_at_most(60) }
+    end
 
 
 
- validates :name, presence: true
- validates :name, length: { minimum: 3, maximum: 60 }
- validate :cpf_is_valid?
+
+
+
+
+
+
+
+
+  end
+end
