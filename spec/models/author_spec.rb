92f3aa0735626f0b74571c3607ba32524976822e
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-03-04 09:51:33 -0300

    feature(tests):book model tests done

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -16,19 +16,20 @@ RSpec.describe Author, type: :model do
       expect(author_type.name).to eq("de Assis, Machado")
     end
 
-    # FIELD NAME REQUIRED
+    # READ: field name required
     describe 'Required validations' do
      it { should validate_presence_of(:name) }
     end
 
-    # MIN AND MAX NAME LENGTH REQUIRED
+    # READ: length min. and max. field title
     describe 'Maximum field size' do
       it { should validate_length_of(:name).is_at_least(3) }
       it { should validate_length_of(:name).is_at_most(60) }
     end
 
-     # DELETE
-     it 'Delete Author' do
+
+    # DELETE
+    it 'Delete Author' do
       author_type = Author.create(name: "Érico Veríssimo")
       author_type.destroy
       expect{ author_type.reload }.to raise_error(ActiveRecord::RecordNotFound)
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,38 +5,37 @@ RSpec.describe Book, type: :model do
 
     let(:current_date) { Date.current }
 
-    # create
+    # CREATE
     it 'Create Book' do
-      book_type = Book.create(title: "Clean Code", published_at: current_date , author_id: 1)
-      expect(book_type.title).to eq("Clean Code")
+      book_type = Book.create(title: "Crime e Castigo", published_at: current_date , author_id: 1)
+      expect(book_type.title).to eq("Crime e Castigo")
       expect(book_type.published_at).to eq(current_date)
       expect(book_type.author_id).to eq(1)
     end
 
-    # update
+    # UPDATE
     it 'Update Book' do
-      book_type = Book.create(title: "Clean Code", published_at: current_date , author_id: 1)
-      book_type.update(title: "C. Code")
-      expect(book_type.title).to eq("C. Code")
+      book_type = Book.create(title: "Crime e Castigo", published_at: current_date , author_id: 1)
+      book_type.update(title: "Crime and Punishment")
+      expect(book_type.title).to eq("Crime and Punishment")
     end
 
-    # field title required
+    # READ: field title required
     describe 'Required validations' do
       it { should validate_presence_of(:title) }
     end
 
-    # length min. and max. field title
+    # READ: length min. and max. field title
     describe  'Min and max. field size' do
       it { should validate_length_of(:title).is_at_least(3) }
       it { should validate_length_of(:title).is_at_most(100) }
     end
 
-    # delete book
+    # DELETE
     it 'Delete book' do
-      book_type = Book.create(title: "Clean Code", published_at: current_date , author_id: 1)
+      book_type = Book.create(title: "Crime and Punishment", published_at: current_date , author_id: 1)
       book_type.destroy
       expect{ book_type.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
-
   end
 end
